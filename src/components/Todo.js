95f3499bcd900ef8f5ff98d0251601fcfbd6badc
syedansar <syedansar6666@gmail.com>
2022-06-09 13:54:54 +0530

    bugs solved

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -15,12 +15,12 @@ const Todo = ({tasks,setTasks}) => {
   return (
         <div className='flex flex-col md:flex-row mt-10'>
           <div className='flex flex-col'>
-        <TodoItem heading={'Active Todos'} active={active} tasks={tasks} setTasks={setTasks} sort={'<'}/>
+        <TodoItem heading={'Active Todos'} active={active} tasks={tasks} setTasks={setTasks} line={'capitalize'}/>
         { active.length === 0 && <h1 className='font-semibold text-xs mb-3 text-red-500 md:w-[200px]'>Create Your Tasks</h1>}
           </div>
           <hr className='w-[60%] mx-auto my-10 md:hidden' />
           <div className='flex flex-col md:ml-32'>
-        <TodoItem heading={'Completed Todos'} active={completedTasks} tasks={tasks} setTasks={setTasks} sort={'>'}/>
+        <TodoItem heading={'Completed Todos'} active={completedTasks} tasks={tasks} setTasks={setTasks} />
         { completedTasks.length === 0 && <h1 className='font-semibold text-xs mb-3 text-green-500 w-[180px] mx-auto'>The key is in not spending time, but in investing it.</h1>}
           </div>
     </div>
